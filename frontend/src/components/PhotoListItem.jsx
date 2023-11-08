65fe8c1829a1b1e3996ba8c003f939ee6194f339
Faiem-qy <f.quanyoung@gmail.com>
2023-11-07 19:35:14 -0500

    add sass styling to photolistitem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,22 +2,32 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
 const PhotoListItem = (props) => {
   /* Insert React */
   const { username, imageSource, id, location, profile } = props;
 
-return (
-  <div className="photo-list-item">
-    <img src={imageSource} alt={`Photo ${id}`}/>
-    <div className="photo-info">
-      <img src={profile} alt={`Profile of ${username}`}/>
-      <h3>{username}</h3>
-      <p>{location.city}, {location.country}</p>
+  return (
+    <div className="photo-list__item">
+      <img
+        className="photo-list__image"
+        src={imageSource}
+        alt={`Photo ${id}`}
+      />
+      <div className="photo-list__user-details">
+        <img
+          className="photo-list__user-profile"
+          src={profile}
+          alt={`Profile of ${username}`}
+        />
+        <div className="photo-list__user-info">
+          <h3>{username}</h3>
+          <p className="photo-list__user-location">
+            {location.city}, {location.country}
+          </p>
+        </div>
+      </div>
     </div>
-  </div>
-)
-
+  );
 };
 
 export default PhotoListItem;
