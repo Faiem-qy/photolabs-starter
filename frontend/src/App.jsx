18b45920adb0733a239bd36171eeb9280121daeb
Faiem-qy <f.quanyoung@gmail.com>
2023-11-07 20:15:51 -0500

    add variable to render data three times

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
-import PhotoList from 'components/PhotoList';
+import PhotoListItem from "./components/PhotoListItem";
+import "./App.scss";
+import PhotoList from "./components/PhotoList";
 
-
-const data = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
+const data = [
+  {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
   },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+];
 
+// add variable to reuse photos template
+const photos = new Array(3).fill(0);
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem key={data.id} username={data.username} imageSource={data.imageSource} id={data.id} location={data.location} profile={data.profile} /> 
+       {photos.map((_, index) => (
+        <PhotoListItem
+          key={index + 1}
+          username={data[0].username}
+          imageSource={data[0].imageSource}
+          id={data[0].id}
+          location={data[0].location}
+          profile={data[0].profile}
+        />
+      ))}
     </div>
   );
 };
