b50c72fe0060890836f0183ab07de72d31c594fa
Faiem-qy <f.quanyoung@gmail.com>
2023-11-08 22:24:20 -0500

    move nav and photolist to homeroute, import to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,7 @@
 import React from "react";
 
-import PhotoListItem from "./components/PhotoListItem";
 import "./App.scss";
-import PhotoList from "./components/PhotoList";
-import TopNavigationBar from "components/TopNavigationBar";
-
-// const data = [
-//   {
-//     id: "1",
-//     location: {
-//       city: "Montreal",
-//       country: "Canada",
-//     },
-//     imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-//     username: "Joe Example",
-//     profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-//   },
-// ];
+import HomeRoute from "routes/HomeRoute";
 
 // add variable to reuse photos template
 const photos = new Array(3).fill(0);
@@ -25,19 +10,7 @@ const photos = new Array(3).fill(0);
 const App = () => {
   return (
     <div className="App">
-      <TopNavigationBar />
-      <PhotoList/>
-       {/* {photos.map((_, index) => (
-        <PhotoListItem
-          key={index + 1}
-          username={data[0].username}
-          imageSource={data[0].imageSource}
-          id={data[0].id}
-          location={data[0].location}
-          profile={data[0].profile}
-        />
-        ))} */}
-       
+      <HomeRoute/>
     </div>
   );
 };
