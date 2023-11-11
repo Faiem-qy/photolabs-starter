43af0b41a428bac8e66d29f5a0985bc00d788b20
Faiem-qy <f.quanyoung@gmail.com>
2023-11-10 21:34:18 -0500

    display mock data - topics and photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import "./App.scss";
 import HomeRoute from "routes/HomeRoute";
-import photos from "mocks/photos";
-import topics from "mocks/topics";
 
 // add variable to reuse photos template
 // const photos = new Array(3).fill(0);
@@ -11,31 +9,8 @@ import topics from "mocks/topics";
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
-    // Topics
     <div className="App">
-      <div className="topics-container">
-        <h2>Topics</h2>
-        <ul>
-          {topics.map((topic) => (
-            <li key={topic.id}>
-              <a href={`/topics/${topic.slug}`}>{topic.title}</a>
-            </li>
-          ))}
-        </ul>
-      </div>
-      {/* photos */}
-      {photos.map((photo) => (
-        <div key={photo.id}>
-          <img src={photo.urls.full} alt={`Photo ${photo.id}`} />
-          {Object.values(photo.similar_photos).map((similarPhoto) => (
-            <img
-              key={similarPhoto.id}
-              src={similarPhoto.urls.full}
-              alt={`Similar Photo ${similarPhoto.id}`}
-            />
-          ))}
-        </div>
-      ))}
+      <HomeRoute/>
     </div>
   );
 };
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,35 +2,36 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
+import topics from "mocks/topics";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+// const sampleDataForTopicList = [
+//   {
+//     id: "1",
+//     slug: "topic-1",
+//     title: "Nature",
+//   },
+//   {
+//     id: "2",
+//     slug: "topic-2",
+//     title: "Travel",
+//   },
+//   {
+//     id: "3",
+//     slug: "topic-3",
+//     title: "People",
+//   },
+// ];
 
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
       {/* Insert React */}
-      {sampleDataForTopicList.map((data, index) => (
+      {topics.map((topic) => (
         <TopicListItem
-          key={data.id}
-          slug={data.slug}
-          title={data.title}
-          id={data.id}
+          key={topic.id}
+          slug={topic.slug}
+          title={topic.title}
+          id={topic.id}
         />
         ))}
     </div>
