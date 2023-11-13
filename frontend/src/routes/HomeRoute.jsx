fb99319fc1dbb3905b4dd2036c868b67797160bf
Faiem-qy <f.quanyoung@gmail.com>
2023-11-12 22:28:18 -0500

    console logged the data of the photo when the photo is clicked and the modal window pops up

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,31 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import '../styles/HomeRoute.scss';
-import TopNavigation from 'components/TopNavigationBar';
-import PhotoList from 'components/PhotoList';
-import PhotoDetailsModal from './PhotoDetailsModal';
+import "../styles/HomeRoute.scss";
+import TopNavigation from "components/TopNavigationBar";
+import PhotoList from "components/PhotoList";
+import PhotoDetailsModal from "./PhotoDetailsModal";
 
 const HomeRoute = () => {
   const [liked, setLiked] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
   const [selectedPhoto, setSelectedPhoto] = useState(null);
 
-  const openModal = () => {
+  const openModal = (photoData) => {
+    setSelectedPhoto(photoData);
     setModalOpen(true);
+    console.log("Selected photo data", photoData);
+    // added console.log to log the data of the photo when the photo is clicked and the modal is open
   };
 
   const closeModal = () => {
     setModalOpen(false);
   };
-// function to close modal
-  
-
+  // function to close modal
 
   return (
     <div className="home-route">
       <TopNavigation liked={liked} setLiked={setLiked} />
       <PhotoList liked={liked} setLiked={setLiked} openModal={openModal} />
-      <PhotoDetailsModal isOpen={modalOpen} onClose={closeModal}/> 
+      <PhotoDetailsModal isOpen={modalOpen} onClose={closeModal} />
       {/* added functionality to close modal */}
     </div>
   );
